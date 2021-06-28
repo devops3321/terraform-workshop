9076c04cc81a121d06fd27970331d00b9ccd2915
Chetan <chetan.talwaar@gmail.com>
2021-06-28 15:52:08 +0530

    Lab3 Added

--- Lab-3-Multiple-Resources/main.tf
+++ Lab-3-Multiple-Resources/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "mysg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = "0.0.0.0/0"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
@@ -23,7 +23,6 @@ resource "aws_security_group" "mysg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
 }

7d9cba278b8c9fb3c671538e7154a2864180025d
Chetan <chetan.talwaar@gmail.com>
2021-06-29 10:48:28 +0530

    Output added in Lab 5

--- Lab-5-S3-with-object/main.tf
+++ Lab-5-S3-with-object/main.tf
@@ -13,3 +13,8 @@ resource "aws_s3_bucket" "mybucket"{
 	bucket = "YOUR_BUCKET_NAME"
 	acl = "public-read"
 }
+output "object_url"{
+	description = "URL"
+	value = "https://${aws_s3_bucket.mybucket.bucket_domain_name}/${aws_s3_bucket_object.myobject.key}"
+}
+

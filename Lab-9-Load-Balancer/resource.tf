1fc4ffaaca638fb76d63909f2cda6d6e3b7370c9
Chetan <chetan.talwaar@gmail.com>
2021-06-29 15:01:54 +0530

    Typos Fixed

--- Lab-9-Load-Balancer/resource.tf
+++ Lab-9-Load-Balancer/resource.tf
@@ -1,21 +1,21 @@
 resource "aws_elb" "mylb"{
     name = var.elb_name
-    availabilty_zones = var.az
-    listner{
+    availability_zones = var.az
+    listener{
         instance_port = 80
         instance_protocol = "http"
         lb_port = 80
         lb_protocol = "http"
     }
     health_check{
-        healthy_thershold = 2
-        unhealthy_thershold = 2
+        healthy_threshold = 2
+        unhealthy_threshold = 2
         timeout = var.timeout
         interval = 10
         target = "HTTP:80/"
     }
     cross_zone_load_balancing = true
-    idle_timoout = var.timeout
+    idle_timeout = var.timeout
     connection_draining = true
     connection_draining_timeout = var.timeout
     tags = {

46f86cffdb7101c81d0c67a9a795506091a6676f
Chetan <chetan.talwaar@gmail.com>
2021-06-30 14:31:41 +0530

    Lab 16 Remote Provisioner

---
+++ Lab-16-Remote-Provisioner/security_group.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "mysg" {
+  name        = "Custom Security Group"
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
---
+++ Lab-16-Remote-Provisioner/vm.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "myserver"{
+	ami = "YOUR_AMI_ID"
+	instance_type = "t2.micro"
+	key_name = aws_key_pair.syne-lab-terra.key_name
+	tags = {
+		Name = "Terraform Lab for Remote Provisoner"
+	}
+	vpc_security_group_ids = [aws_security_group.mysg.id]
+	connection {
+		type = "ssh"
+		user = "ubuntu"
+		private_key = file("YOUR_KEY_FILE_NAME")
+		host = self.private_ip
+	}
+	provisioner "remote-exec" {
+		inline = [
+			"sudo apt-get update && sudo apt-get install apache2 -y && echo Sample > /home/ubuntu/test"
+		]
+	}
+}

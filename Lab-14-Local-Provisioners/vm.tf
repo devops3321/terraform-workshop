349143c7732ac0394746ba21ad17e2db2c679a2a
Chetan <chetan.talwaar@gmail.com>
2021-06-30 12:16:30 +0530

    Lab 14 - Local Provisioner

---
+++ Lab-14-Local-Provisioners/vm.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "myserver"{
+	ami = "YOUR_AMI_ID"
+	instance_type = "t2.micro"
+	tags = {
+		Name = "Terraform In Action"
+	}
+	provisioner "local-exec"{
+		command = "echo aws_instance.myserver.private_ip > output.txt"
+	}
+}
